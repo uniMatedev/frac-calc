220b181ee38c0ec7debddfe31cc628152c4cb0b6
zachmbanks <zachmbanks@gmail.com>
2020-07-10 15:32:07 -0700

    Initial commit

---
+++ fracFunc.js
@@ -0,0 +1,39 @@
+import Fraction from "fraction.js";
+
+let [roWidth, roHeight, outsideFrameWidth, outsideFrameHeight, shimGap] = Array(
+  5
+).fill(0);
+
+function calculateFrameWidth() {
+  let shimGapSelct = document.querySelector("#shim-gap");
+  shimGap = shimGapSelct.options[shimGapSelct.selectedIndex].value;
+  let outsideFrameWidth = roWidth - shimGap * 2;
+  let outsideFrameHeight = roHeight - shimGap;
+  // let oFrameWidthFraction = new Fraction(outsideFrameWidth)
+  document.querySelector(
+    "#display"
+  ).innerHTML = `The oustside Frame dimentions are ${nerestUsableFraction(
+    outsideFrameWidth,
+    16
+  )} x ${nerestUsableFraction(outsideFrameHeight, 16)}`;
+}
+
+function nerestUsableFraction(decimalNumber, denominator) {
+  let sixteenth = new Fraction(1 / denominator);
+  let x = Math.round(decimalNumber * sixteenth.d);
+  let newFrac = new Fraction(x / sixteenth);
+  let numeratorNotReduced = newFrac.n / newFrac.d;
+  let numeratorReduced = numeratorNotReduced / denominator;
+  let final = new Fraction(numeratorReduced / denominator);
+  console.log(final.toFraction());
+  let wholeNumberReduce = final.n / final.d;
+  console.log(wholeNumberReduce);
+  let dec = wholeNumberReduce % 1;
+  let int = Math.floor(wholeNumberReduce);
+  let lastFrac = new Fraction(dec).toFraction();
+  console.log(lastFrac);
+  let lastNumber = int + " " + lastFrac;
+  console.log(lastNumber);
+  return lastNumber;
+  //
+}
